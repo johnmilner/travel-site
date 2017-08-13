d7c799429cf7766f56c959dda40faf96aeb58892
John <john.milner@mac.com>
2017-08-13 16:35:39 -0700

    Menu icon animation completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,22 +1,4 @@
-var $ = require('jquery');
-// var Person = require('./modules/Person');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-	payTaxes() {
-		console.log(this.name + " now owes $0 in taxes.");
-	}
-
-}
-
-
-alert("ABC 321");
-
-var john = new Person("Johnny Guitar", "blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "orange");
-jane.greet();
-jane.payTaxes();
-
-$("h1").remove();
+// mobileMenu is a new instance of the MobileMenu class
+var mobileMenu = new MobileMenu();
