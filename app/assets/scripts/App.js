148eed974eea4bcfe4c85ba877fed1b0f21f7466
John <john.milner@mac.com>
2017-08-13 20:22:39 -0700

    Completed Feature and Testimonial section waypoint fade-in scroll animation

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,17 @@
 import MobileMenu from './modules/MobileMenu';
-
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 // mobileMenu is a new instance of the MobileMenu class
 var mobileMenu = new MobileMenu();
+
+// revealOnScroll is a new instance of the RevealOnScroll class
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
+
+// import MobileMenu from './modules/MobileMenu';
+// import RevealOnScroll from './modules/RevealOnScroll';
+// import $ from 'jquery';
+
+// var mobileMenu = new MobileMenu();
+// new RevealOnScroll($(".feature-item"), "85%");
+// new RevealOnScroll($(".testimonial"), "60%");
